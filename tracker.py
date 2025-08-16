bb577b305b02c16b51c28e422ba4280e4d20f5b4
Adrian-rospx <adrianrosu5400@gmail.com>
2025-08-16 23:34:19 +0300

    added: yellow color filtering mode

--- tracker.py
+++ tracker.py
@@ -1,37 +1,69 @@
+"""
+Color filtering project using OpenCV
+"""
 import cv2
 import numpy as np
 
 import os
 import sys
 
+from enum import Enum
+
 # check different source input
 s: int = 0
 if len(sys.argv) > 1:
     s = sys.argv[1]
-
+# start video caputure
 source: cv2.VideoCapture = cv2.VideoCapture(s)
 
 # create the window
 window: str = "Color Tracker"
 cv2.namedWindow(window, cv2.WINDOW_NORMAL)
 
-# main loop
+result = None
 running: bool = True
 
+class ThresholdMode(Enum):
+    # enums for color modes
+    NONE = 0
+    YELLOW = 1
+mode = ThresholdMode.YELLOW
+
+def yellowMask(img: cv2.typing.MatLike) -> cv2.typing.MatLike:
+    # masking function
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower = np.array([15, 80, 108])
+    upper = np.array([40, 255, 255])
+
+    return cv2.inRange(img_hsv, lower, upper)
+
+# main loop
 while running:
     ret, frame = source.read()
     if not ret: 
         break
 
-    
+    # process frame
+    match mode:
+        case ThresholdMode.NONE:
+            result = frame
+        case ThresholdMode.YELLOW:
+            y_mask = yellowMask(frame)
+            result = cv2.bitwise_and(frame, frame, mask = y_mask)
 
-    # get the current key (and handle overflow)
+    # handle input
     key: int = cv2.waitKey(1)
-
     if key == ord('q') or key == ord('Q') or key == 27:
-        running = False
+        # quit
+        running = False    
+    elif key == ord('n') or key == ord('N'):
+        mode = ThresholdMode.NONE
+    elif key == ord('y') or key == ord('Y'):
+        mode = ThresholdMode.YELLOW
 
-    cv2.imshow(window, frame)
+    # display
+    cv2.imshow(window, result)
 
 # release memory and close
 source.release()
